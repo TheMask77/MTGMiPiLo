5b80924017db2cf2f00e654ce915f17ef8cfda9b
TheMask77 <m.carissimi4@campus.unimib.it>
2025-05-10 16:27:20 +0200

    Fix barchart max y value

--- components/overview.tsx
+++ components/overview.tsx
@@ -11,11 +11,17 @@ interface OverviewProps {
 }
 
 export function Overview({ data }: OverviewProps) {
+  const cleanedData = data.map(d => ({
+    ...d,
+    wins: Number(d.wins),
+    losses: Number(d.losses),
+  }));
+
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
-        <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
-        <YAxis stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
+      <BarChart data={cleanedData}>
+        <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={true} axisLine={false} />
+        <YAxis stroke="#888888" fontSize={12} tickLine={false} axisLine={false} domain={[0, 'dataMax + 2']}/>
         <Tooltip />
         <Bar dataKey="wins" fill="#4ade80" radius={[4, 4, 0, 0]} />
         <Bar dataKey="losses" fill="#f87171" radius={[4, 4, 0, 0]} />
